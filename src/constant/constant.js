a6bfd54c62135a80bebe8b97d152b3a8bf7df75c
Pavithiran21 <82439522+Pavithiran21@users.noreply.github.com>
2021-09-22 00:16:17 +0800

    Update constant.js

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,6 +1,6 @@
 // export const API_URL="https://reset-passwords.herokuapp.com/user"
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
-export const API_URL="http://localhost:9003"
+export const API_URL="https://shortner-app.herokuapp.com/"
 export const USER_SESSION=cookies.get('user_token')
 export const USER_DETAILS=cookies.get('user_details')
